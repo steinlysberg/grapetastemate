49cbf9b2f66d6cb3bd4eb396dfccd89f24298a73
Stein Lysberg <stein.lysberg@gmail.com>
2018-12-22 17:33:29 +0100

    Added 'new', 'edit' and 'delete' methods. Updated 'show' functionality.

--- app/controllers/wines_controller.rb
+++ app/controllers/wines_controller.rb
@@ -1,15 +1,48 @@
 class WinesController < ApplicationController
+
+  before_action :current_wine, only: [:show, :edit, :update, :destroy]
+
+
   def index
     @wines = Wine.all
   end
 
   def show
-    @wine = Wine.find(params[:id])
-  end
-
-  def new
   end
 
   def edit
   end
+
+  def update
+    @wine.update(wine_params)
+
+    redirect_to wine_path(@wine)
+  end
+
+  def new
+    @wine = Wine.new
+  end
+
+  def create
+    wine = Wine.create(wine_params)
+
+    redirect_to wines_path
+  end
+
+  def destroy
+    @wine.destroy
+
+    redirect_to wines_path
+  end
+
+  private
+
+  def wine_params
+    params.require(:wine).permit(:name, :color, :year, :country, :review)
+  end
+
+  def current_wine
+    @wine = Wine.find(params[:id])
+  end
+
 end
